989a236b2d9f4a34814b225a0cc793ce3dd0ebc8
root <root@ip-172-31-12-10.ec2.internal>
2026-07-07 08:40:17 +0000

    in this i remove the container name properly

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
-docker stop reactci-cdcontainer
-docker rm reactci-cdcontainer
+docker stop reactcontainer
+docker rm reactcontainer
 
 docker build -t reactappim .
 
 docker run -itd --name reactcontainer -p 80:80 reactappim
 
-docker tag reactappim sriraam275/reactappimCICD
-docker push sriraam275/reactappimCICD
+docker tag reactappim sriraam275/reactappimcicd
+docker push sriraam275/reactappimcicd
 
